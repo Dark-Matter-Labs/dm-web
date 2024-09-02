f8ebedc45fdc8bfb39469c20816e423bb0df7c44
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-09-02 16:35:11 +0200

    update packages and replace studio code

--- app/studio/[[...index]]/page.jsx
+++ app/studio/[[...index]]/page.jsx
@@ -1,17 +1,20 @@
-import Head from 'next/head';
-import { NextStudio } from 'next-sanity/studio';
-import { metadata } from 'next-sanity/studio/metadata';
-import config from '../../../sanity.config';
+
+'use client';
+/**
+ * This route is responsible for the built-in authoring environment using Sanity Studio v3.
+ * All routes under /studio will be handled by this file using Next.js' catch-all routes:
+ * https://nextjs.org/docs/routing/dynamic-routes#catch-all-routes
+ *
+ * You can learn more about the next-sanity package here:
+ * https://github.com/sanity-io/next-sanity
+ */
+
+
+import { NextStudio } from 'next-sanity/studio'
+
+import config from '../../../sanity.config'
 
 export default function StudioPage() {
-  return (
-    <>
-      <Head>
-        {Object.entries(metadata).map(([key, value]) => (
-          <meta key={key} name={key} content={value} />
-        ))}
-      </Head>
-      <NextStudio config={config} />
-    </>
-  );
+  console.log(config)
+  return <NextStudio config={config} />
 }
