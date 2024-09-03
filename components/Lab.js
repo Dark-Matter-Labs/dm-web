e5373ab9f82bee75f77f00752e2177d8a6b27127
Gurden <gurden@darkmatterlabs.org>
2024-09-03 16:24:19 +0200

    final adjustments from Vlad

--- components/Lab.js
+++ components/Lab.js
@@ -22,14 +22,14 @@ function Lab({
       }}
       className={classNames(
         activeState ? ' text-white' : ' text-[#A8A8A8]',
-        'flex h-[80px] w-[80px] cursor-crosshair flex-col justify-between px-2 py-[7px] tracking-wide',
+        'flex h-[80px] w-[80px] cursor-crosshair flex-col justify-between pt-[5px] pb-[6.5px] pl-2 pr-[6px] tracking-wide',
       )}
       onMouseOver={() => setActive(true)}
       onMouseLeave={() => setActive(false)}
       onClick={() => setOpen(true)}
     >
       <p className="font-SaansRegular text-[17px] font-normal ">{short}</p>
-      <p className=" font-SaansRegular text-[12px] font-normal leading-[125%]">
+      <p className="font-SaansRegular text-[12px] font-normal leading-[125%]">
         {title}
       </p>
     </animated.div>
