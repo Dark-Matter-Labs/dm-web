a1045394d55f9b3a28985cf77e4285f1a543366f
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-09-03 16:45:08 +0200

    add revalidate route

--- app/(front-end)/team/page.js
+++ app/(front-end)/team/page.js
@@ -11,7 +11,9 @@ const dmlienQuery = `
 `;
 
 export async function getDmliens() {
-  const dmliens = await client.fetch(dmlienQuery, { cache: 'no-store' });
+  const dmliens = await client.fetch(dmlienQuery, {
+    next: { tags: ['dmlien', 'initiative'] },
+  },);
   return dmliens;
 }
 
