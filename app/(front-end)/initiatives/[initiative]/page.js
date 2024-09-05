fbf7c80988a2f149c86967947306fdd302a33a90
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-09-05 12:03:34 +0200

    make pages

---
+++ app/(front-end)/initiatives/[initiative]/page.js
@@ -0,0 +1,9 @@
+
+
+export default async function InitiativePage() {
+    return (
+        <div>
+            hello
+        </div>
+    )
+}
--- app/(front-end)/initiatives/page.js
+++ app/(front-end)/initiatives/page.js
@@ -1,3 +1,17 @@
-export default function Initiatives() {
-  return <div></div>;
+import { sanityFetch } from "@/sanity/lib/client"
+
+const INITIATIVES_QUEARY = `
+*[_type == initiative] {
+...}
+`
+
+
+export default async function Initiatives() {
+  const initiatives = await sanityFetch({INITIATIVES_QUEARY, tags: ['initiative']})
+  console.log(initiatives)
+  return (
+    <div>
+      Hello
+    </div>
+  )
 }
