6ab0d7ca2299b160155325c92e9de52d64ca0a7e
Gurden <gurden@darkmatterlabs.org>
2024-09-05 12:31:17 +0200

    improve load colors

--- utils/imageLoad.js
+++ utils/imageLoad.js
@@ -2,14 +2,14 @@ export const convertImage = (w, h) => `
 <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
   <defs>
     <linearGradient id="g">
-      <stop stop-color="#111112" offset="20%" />
-      <stop stop-color="#111112" offset="50%" />
-      <stop stop-color="#111112" offset="70%" />
+      <stop stop-color="#E2E2E2" offset="20%" />
+      <stop stop-color="#585858" offset="50%" />
+      <stop stop-color="#7D7D7D" offset="70%" />
     </linearGradient>
   </defs>
-  <rect width="${w}" height="${h}" fill="#333" />
+  <rect width="${w}" height="${h}" fill="#111112" />
   <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="0.3s" repeatCount="indefinite"  />
 </svg>`;
 
 export const toBase64 = (str) =>
