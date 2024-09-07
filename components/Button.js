1fd5e3d23f00d1798a11ad451926dacab32e03d4
Gurden <gurden@darkmatterlabs.org>
2024-09-07 14:51:56 +0200

    improve button layout

--- components/Button.js
+++ components/Button.js
@@ -2,16 +2,16 @@ import Link from 'next/link';
 
 export default function DMButton({ children, href, internal }) {
   return (
-    <div className="duration-2000 border-2 border-white px-2 pb-1.5 pt-0.5 text-white hover:cursor-crosshair hover:bg-white hover:text-black">
+    <div className="duration-2000 border-2 border-white pr-[9px] pl-[8px] pb-[7px]  text-white hover:cursor-crosshair hover:bg-white hover:text-black">
       {internal ? (
-        <Link href={href} className="flex flex-col items-end">
-          <p className="font-SaansMed text-[18px]">{'↗'}</p>
-          <p className="font-SaansMed text-2xl leading-[1em]">{children}</p>
+        <Link href={href} className="flex flex-col justify-start items-end gap-[1px]">
+          <p className="font-SaansRegular text-[16px] leading-[1em]">{'↗'}</p>
+          <p className="font-SaansRegular text-2xl leading-[1em]">{children}</p>
         </Link>
       ) : (
-        <a href={href} className="flex flex-col items-end">
-          <p className="font-SaansMed text-[18px]">{'↗'}</p>
-          <p className="font-SaansMed text-2xl leading-[1em]">{children}</p>
+        <a href={href} className="flex flex-col justify-start items-end gap-[1px]">
+          <p className="font-SaansRegular text-[16px] leading-[1em]">{'↗'}</p>
+          <p className="font-SaansRegular text-2xl leading-[1em]">{children}</p>
         </a>
       )}
     </div>
