cd674cb43ea2b50d207f056fe59fe4769b0cd29d
Gurden <gurden@darkmatterlabs.org>
2024-09-07 21:28:54 +0200

    feed responsive

--- components/FeedItem.js
+++ components/FeedItem.js
@@ -5,26 +5,28 @@ export default function FeedItem({ item }) {
   return (
     <div
       key={item.title}
-      className="flex h-full w-[1200px] items-start justify-between border-b border-b-[#353535]"
+      className="flex h-full flex-col items-center justify-center gap-[20px] border-b border-b-[#353535] sm:flex-row sm:items-start sm:justify-between"
     >
-      <div className="flex w-[470px] items-center justify-center py-[34px]">
+      <div className="flex w-full items-center justify-center py-[20px] sm:w-[247px] sm:py-[34px] md:w-[470px]">
         <Image
           src={urlForImage(item.image)}
           alt="feed item image"
+          className="h-full w-full sm:h-[140px] md:h-[266px] md:w-[470px]"
           width={0}
           height={0}
           sizes="100vw"
-          style={{ width: '100%', height: '266px' }}
         />
       </div>
-      <div className="flex h-[330px] w-[690px] flex-col items-start justify-between py-[34px]">
-        <div className="flex flex-col items-start justify-start gap-[2px]">
-          <h2 className="font-SaansRegular text-5xl leading-[36px] text-white">
+      <div className="flex h-[210px] w-full flex-col items-start justify-between py-[20px] sm:w-[470px] sm:py-[34px] md:h-[340px] md:w-[690px]">
+        <div className="flex flex-col items-start justify-between gap-[2px] md:justify-start">
+          <h2 className="max-w-[400px] font-SaansRegular text-4xl leading-[28px] text-white md:max-w-full md:text-5xl md:leading-[36px]">
             {item.title}
           </h2>
-          <h3 className="p-body2 text-grey-3">{item.subtitle}</h3>
+          <h3 className="p-body2 hidden text-grey-3 md:block">
+            {item.subtitle}
+          </h3>
         </div>
-        <div className="flex items-end justify-start">
+        <div className="flex items-start justify-start">
           <div className="flex w-[120px] flex-col items-start justify-center gap-[6px]">
             <p className="nav-xl uppercase text-[#595959]">Type</p>
             <p className="nav-xl capitalize text-grey-3">{item.type}</p>
@@ -40,7 +42,7 @@ export default function FeedItem({ item }) {
           </div>
           <div className="flex flex-col items-start justify-center gap-[6px]">
             <p className="nav-xl uppercase text-[#595959]">Units</p>
-            <div className="flex gap-0.5">
+            <div className="flex flex-col gap-0.5 md:flex-row">
               {item.labs && (
                 <p className="nav-xl capitalize text-grey-3">
                   {item.labs[0].title}
